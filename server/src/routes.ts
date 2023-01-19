25084dc808b99f9178af75fb9fcaf31359d12706
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2023-01-19 16:35:36 -0300

    [x] ainda preciso finalizar as rotas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,46 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "./lib/prisma";
+import { z } from "zod";
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const habits = await prisma.habit.findMany();
-    return habits;
+  app.post("/habits", async (request) => {
+    const createHabitsBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitsBody.parse(request.body);
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((week_day) => ({ week_day: week_day })),
+        },
+      },
+    });
+  });
+  app.get("/day", async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+    const { date } = getDayParams.parse(request.query);
+    const weekDay = dayjs(date).get("day");
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
   });
 }
