60f7c52ba22068ae823be63f542571cf3e8fa2c5
Guilherme Henrique Couto de Jesus <guicoutoo@outlook.com>
2023-01-22 11:40:32 -0300

    [x] criacao de habitos, e detalhes do dia

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,8 +27,13 @@ export async function appRoutes(app: FastifyInstance) {
     const getDayParams = z.object({
       date: z.coerce.date(),
     });
+
     const { date } = getDayParams.parse(request.query);
-    const weekDay = dayjs(date).get("day");
+
+
+    const parsedDate = dayjs(date).startOf("day");
+    const weekDay = parsedDate.get("day");
+     
 
     const possibleHabits = await prisma.habit.findMany({
       where: {
@@ -42,5 +47,21 @@ export async function appRoutes(app: FastifyInstance) {
         },
       },
     });
+   const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include:{
+        dayHabits:true
+      }
+   
+
+   })
+   const completedHabits = day?.dayHabits.map(dayHabit=>{
+    return dayHabit.habit_id
+   })
+
+
+    return { possibleHabits, completedHabits };
   });
 }
